2f283e21d2106519c9e74eaa0029bc5956c24094
Parsatag462 <mr.nitsuji462@gmail.com>
2022-10-15 15:20:09 -0400

    Art

---
+++ ASCIIArt/src/Art2.java
@@ -0,0 +1,14 @@
+public class Art2 {
+    public static void main(String[] args) {
+        // it's a farm house that is spaces long
+        System.out.println("  ___________  ");
+        System.out.println(" /___________\\  ");
+        System.out.println(" |         __|___________");
+        System.out.println(" |        /              \\ ");
+        System.out.println(" |       /________________\\");
+        System.out.println(" |       |     _______    |");
+        System.out.println(" |       |     |__|__|    |");
+        System.out.println(" |       |     |__|__|    |");
+        System.out.println(" |_______|_____|__|__|____|");
+    }
+}
