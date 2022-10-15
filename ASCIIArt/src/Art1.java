46fe53dff28ae5ff2259d29d4323ddc7d9b083c4
Parsatag462 <mr.nitsuji462@gmail.com>
2022-10-15 14:52:59 -0400

    Art

---
+++ ASCIIArt/src/Art1.java
@@ -0,0 +1,12 @@
+public class Art1 {
+    public static void main(String[] args) {
+        System.out.println("  xxxx  ");
+        System.out.println(" x    x ");
+        System.out.println("x x  x x");
+        System.out.println("x      x");
+        System.out.println("x x  x x");
+        System.out.println("x  xx  x");
+        System.out.println(" x    x ");
+        System.out.println("  xxxx  ");
+    }
+}
